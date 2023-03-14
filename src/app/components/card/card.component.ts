a020abc94f3a06f238286369350df263c962755f
jailcomfranssa <jail_son_0@hotmail.com>
2023-03-14 18:48:38 -0300

    repasando paramentro de componente pai para filho

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  @Input()
+  gameType:string = "Digital Ps4"
+
+  @Input()
+  gamePrice:string="R$: 399,90"
+
+  @Input()
+  gameCover:string = ""
+
+  @Input()
+  gameLabel:string=""
 
   constructor() { }
 
